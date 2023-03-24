b225222d4f984959c7194085a0f40890f02b5578
Suhadak Akbar <ggsuha@gmail.com>
2023-03-24 16:10:35 +0800

    Show navbar

--- resources/js/Layout/Navbar.vue
+++ resources/js/Layout/Navbar.vue
@@ -1,28 +1,30 @@
+<script setup lang="ts">
+import { Link } from '@inertiajs/vue3';
+</script>
+
 <template>
   <nav class="navigation">
     <section class="container">
-      <!-- <a class="navigation-title" href="/">
-        suha
-      </a> -->
+      <Link class="navigation-title" href="/">suha</Link>
 
-      <!-- <input type="checkbox" id="menu-toggle" />
+      <input type="checkbox" id="menu-toggle" />
       <label class="menu-button float-right" for="menu-toggle">
-        <i class="fa fa-bars fa-fw" aria-hidden="true"></i>
-      </label>
-      <ul class="navigation-list">
-        <li class="navigation-item">
+      <i class="fa fa-bars fa-fw" aria-hidden="true"></i>
+    </label>
+    <ul class="navigation-list">
+      <!-- <li class="navigation-item">
           <a class="navigation-link" href="about.html">About</a>
         </li>
 
-        <li class="navigation-item">
-          <a class="navigation-link" href="posts.html">Blog</a>
-        </li>
+          <li class="navigation-item">
+            <a class="navigation-link" href="posts.html">Blog</a>
+          </li> -->
 
         <li class="navigation-item">
-          <a class="navigation-link" href="projects.html">Projects</a>
-        </li>
+        <Link class="navigation-link" href="/project">Projects</Link>
+      </li>
 
-        <li class="navigation-item">
+      <!-- <li class="navigation-item">
           <a class="navigation-link" href="contact.html">Contact me</a>
         </li>
 
@@ -30,10 +32,10 @@
           <span>|</span>
         </li>
 
-        <li class="navigation-item">
-          <a href="pt-br.html">&#127463;&#127479;</a>
-        </li>
-      </ul> -->
+          <li class="navigation-item">
+            <a href="pt-br.html">&#127463;&#127479;</a>
+          </li> -->
+      </ul>
     </section>
   </nav>
 </template>
