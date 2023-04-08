582042351bc6e6bcd17a456b150c96bafc8ae7c8
Suhadak Akbar <ggsuha@gmail.com>
2023-04-08 23:07:58 +0800

    Add google analytic

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,6 +8,21 @@
 
     <link rel="icon" type="image/png" href="{{ asset('favicons/images-favicon-32x32.png') }}" sizes="32x32" />
     <link rel="icon" type="image/png" href="{{ asset('favicons/images-favicon-16x16.png') }}" sizes="16x16" />
+
+    @env('production')
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-M7W57TGZ0D"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-M7W57TGZ0D');
+    </script>
+    @endenv
     @vite('resources/js/app.ts')
     @inertiaHead
 </head>
