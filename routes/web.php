d52e45d8baf259115aa264a8bb94b1aa8cc1cacd
Suhadak Akbar <ggsuha@gmail.com>
2023-06-18 20:52:34 +0800

    Add pinger endpoint

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,7 @@ Route::get('/', function () {
 
 Route::get('/project', [ProjectController::class, 'index']);
 Route::get('/project/{project:slug}', [ProjectController::class, 'show']);
+
+Route::get('ping', function () {
+    return response()->json();
+});
